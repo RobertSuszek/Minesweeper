f830ade2e73728712d4c0c8338abb424dd537713
Robert <robert.suszek96@gmail.com>
2018-01-24 16:53:12 +0100

    Project creation, several basic features.
    Need to handle user input.

---
+++ Minesweeper/Minesweeper/Game.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minesweeper
+{
+    enum Difficulty { easy, medium, hard, nonstandard };
+    enum CellType { bomb = -1, zero, one, two, three, four, five, six, seven, eight, undefined }
+
+    struct Cell
+    {
+        public int x;
+        public int y;
+        public bool isBomb;
+        public int adjacentBombCount;
+    };
+
+    class Game
+    {
+        private int width;
+        private int height;
+        private int bombCount;
+        private Cell[][] board;
+
+        public int GetWidth() { return width; }
+        public int GetHeight() { return height; }
+
+        private CellType GetCellType(Cell cell)
+        {
+            CellType celltype = CellType.undefined;
+
+            switch(cell.adjacentBombCount)
+            {
+                case -1:
+                    celltype = CellType.bomb;
+                    break;
+                case 0:
+                    celltype = CellType.zero;
+                    break;
+                case 1:
+                    celltype = CellType.one;
+                    break;
+                case 2:
+                    celltype = CellType.two;
+                    break;
+                case 3:
+                    celltype = CellType.three;
+                    break;
+                case 4:
+                    celltype = CellType.four;
+                    break;
+                case 5:
+                    celltype = CellType.five;
+                    break;
+                case 6:
+                    celltype = CellType.six;
+                    break;
+                case 7:
+                    celltype = CellType.seven;
+                    break;
+                case 8:
+                    celltype = CellType.eight;
+                    break;
+            }
+            return celltype;
+        }
+
+        public CellType[][] GetBoard()
+        {
+            CellType[][] cellTypeBoard;
+            cellTypeBoard = new CellType[width][];
+            for (int i = 0; i < width; i++)
+                cellTypeBoard[i] = new CellType[height];
+
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                    cellTypeBoard[x][y] = GetCellType(board[x][y]);
+            }
+
+            return cellTypeBoard;
+        }
+
+        private int CalculateAdjacentBombCount(Cell cell)
+        {
+            int bombCount = 0;
+
+            if (cell.isBomb)
+                return -1;
+
+            if (cell.x > 0 && cell.y > 0)
+            {
+                if (board[cell.x - 1][cell.y - 1].isBomb)
+                    bombCount++;
+            }
+            if (cell.y > 0)
+            {
+                if (board[cell.x][cell.y - 1].isBomb)
+                    bombCount++;
+            }
+            if (cell.x < width-1 && cell.y > 0)
+            {
+                if (board[cell.x + 1][cell.y - 1].isBomb)
+                    bombCount++;
+            }
+            if (cell.x > 0)
+            {
+                if (board[cell.x - 1][cell.y].isBomb)
+                    bombCount++;
+            }
+            if (cell.x < width - 1)
+            {
+                if (board[cell.x + 1][cell.y].isBomb)
+                    bombCount++;
+            }
+            if (cell.x > 0 && cell.y < height - 1)
+            {
+                if (board[cell.x - 1][cell.y + 1].isBomb)
+                    bombCount++;
+            }
+            if (cell.y < height - 1)
+            {
+                if (board[cell.x][cell.y + 1].isBomb)
+                    bombCount++;
+            }
+            if (cell.x < width - 1 && cell.y < height - 1)
+            {
+                if (board[cell.x + 1][cell.y + 1].isBomb)
+                    bombCount++;
+            }
+
+
+            return bombCount;
+        }
+
+        private void SetupAdjacentBombCounts()
+        {
+            for (int x = 0; x < width; x++)
+            {
+                for(int y = 0; y < height; y++)
+                    board[x][y].adjacentBombCount = CalculateAdjacentBombCount(board[x][y]);
+            }
+        }
+
+        private void SetupBombs()
+        {
+            Random random = new Random();
+            int randomX;
+            int randomY;
+            int i = 0;
+
+            while (i < bombCount)
+            {
+                randomX = random.Next(0, width);
+                randomY = random.Next(0, height);
+
+                if (!board[randomX][randomY].isBomb)
+                {
+                    board[randomX][randomY].isBomb = true;
+                    board[randomX][randomY].adjacentBombCount = -1;
+                    i++;
+                }
+            }
+
+        }
+
+        private void InitializeBoard()
+        {
+            board = new Cell[width][];
+            for (int x = 0; x < width; x++)
+            {
+                board[x] = new Cell[height];
+                for (int y = 0; y < height; y++)
+                {
+                    board[x][y].x = x;
+                    board[x][y].y = y;
+                    board[x][y].isBomb = false;
+                    board[x][y].adjacentBombCount = 0;
+                }
+            }
+        }
+
+        private void SetDifficulty(Difficulty diff, int Width, int Height, int BombCount)
+        {
+            switch (diff)
+            {
+                case Difficulty.easy:
+                    width = 8;
+                    height = 8;
+                    bombCount = 10;
+                    break;
+                case Difficulty.medium:
+                    width = 16;
+                    height = 16;
+                    bombCount = 40;
+                    break;
+                case Difficulty.hard:
+                    width = 16;
+                    height = 30;
+                    bombCount = 99;
+                    break;
+                case Difficulty.nonstandard:
+                    width = Width;
+                    height = Height;
+                    bombCount = BombCount;
+                    break;
+            }
+        }
+
+        public Game(Difficulty diff, int Width = 8, int Height = 8, int BombCount = 10)
+        {
+            SetDifficulty(diff, Width, Height, BombCount);
+            InitializeBoard();
+            SetupBombs();
+            SetupAdjacentBombCounts();
+        }
+    }
+}
---
+++ Minesweeper/Minesweeper/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minesweeper
+{
+    class Program
+    {
+        
+
+        static int Main()
+        {
+            Game game = new Game(Difficulty.medium);
+            CellType[][] board = game.GetBoard();
+
+            for (int x = 0; x < game.GetWidth(); x++)
+            {
+                for (int y = 0; y < game.GetHeight(); y++)
+                {
+                    DisplayBoardElem(board[x][y]);
+                }
+                Console.WriteLine();
+            }
+
+            return 0;
+        }
+
+        static void DisplayBoardElem(CellType cell)
+        {
+            switch (cell)
+            {
+                case CellType.bomb:
+                    Console.Write("B ");
+                    break;
+                case CellType.zero:
+                    Console.Write("0 ");
+                    break;
+                case CellType.one:
+                    Console.Write("1 ");
+                    break;
+                case CellType.two:
+                    Console.Write("2 ");
+                    break;
+                case CellType.three:
+                    Console.Write("3 ");
+                    break;
+                case CellType.four:
+                    Console.Write("4 ");
+                    break;
+                case CellType.five:
+                    Console.Write("5 ");
+                    break;
+                case CellType.six:
+                    Console.Write("6 ");
+                    break;
+                case CellType.seven:
+                    Console.Write("7 ");
+                    break;
+                case CellType.eight:
+                    Console.Write("8 ");
+                    break;
+            }
+        }
+    }
+}
